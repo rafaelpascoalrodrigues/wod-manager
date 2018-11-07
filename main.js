1b3240481ec53f96f2edf7ef172f990139079ca8
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2018-11-07 13:21:46 -0200

    Add memory of window last position and size

--- main.js
+++ main.js
@@ -1,16 +1,64 @@
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 
 const {app, BrowserWindow} = electron;
 
 
 let mainWindow;
+let mainWindowData = null;
+let mainWindowDataFile = path.join(app.getPath('userData'), 'window.json');
+
+
+function loadMainWindowConfig() {
+    var savedData;
+    
+    if (!fs.existsSync(mainWindowDataFile)) {
+        return {};
+    }
+    
+    try {
+        savedData = JSON.parse(fs.readFileSync(mainWindowDataFile, 'utf-8'));
+    } catch (e) {
+        return {};
+    }
+
+    return savedData;
+}
+
+function saveMainWindowConfig(window) {
+    var saveData = {};
+    
+    try {
+        var bounds = window.getBounds();
+
+        saveData['x'] = bounds.x;
+        saveData['y'] = bounds.y;
+        saveData['width'] = bounds.width;
+        saveData['height'] = bounds.height;
+        saveData['maximized'] = window.isMaximized();
+        
+        fs.writeFileSync(mainWindowDataFile, JSON.stringify(saveData));
+    } catch (e) {
+        return;
+    }
+}
+
 
 // listen for app to be ready
 app.on('ready', function() {
     
-    mainWindow = new BrowserWindow({frame: false, minHeight: 500, minWidth: 350});
+    mainWindoConfig = loadMainWindowConfig();
+    mainWindoConfig['frame'] = false;
+    mainWindoConfig['minWidth'] = 350;
+    mainWindoConfig['minHeight'] = 500;
+
+    mainWindow = new BrowserWindow(mainWindoConfig);
+    if (mainWindoConfig.maximized) {
+        mainWindow.maximize();
+    }
+
     mainWindow.webContents.openDevTools();
 
     mainWindow.on('maximize', function () {
@@ -28,9 +76,14 @@ app.on('ready', function() {
     });
 
 
-    mainWindow.on('closed', function() {
+    mainWindow.on('close', function(e) {
+        saveMainWindowConfig(mainWindow);
+    });
+    
+    mainWindow.on('closed', function(e) {
         app.quit()
     });
+
 
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'app/mainwindow.html'),
